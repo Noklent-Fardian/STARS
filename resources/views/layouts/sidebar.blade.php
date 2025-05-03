2b693000af2efb2fee05bb2a7061f4341891ff4c
taufikdimas <taufikdimasedystara@gmail.com>
2025-05-03 21:52:15 +0700

    feat: sidebar admin

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,100 +1,101 @@
- <!-- Sidebar -->
-    <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon">
-          <img src="RuangAdmin/img/logo/logo2.png">
-        </div>
-        <div class="sidebar-brand-text mx-3">STARS</div>
-      </a>
-      <hr class="sidebar-divider my-0">
-      <li class="nav-item">
-        <a class="nav-link" href="index.html">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Dashboard</span></a>
-      </li>
-      <hr class="sidebar-divider">
-      <div class="sidebar-heading">
-        Features
-      </div>
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBootstrap"
-          aria-expanded="true" aria-controls="collapseBootstrap">
-          <i class="far fa-fw fa-window-maximize"></i>
-          <span>Bootstrap UI</span>
-        </a>
-        <div id="collapseBootstrap" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Bootstrap UI</h6>
-            <a class="collapse-item" href="alerts.html">Alerts</a>
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="dropdowns.html">Dropdowns</a>
-            <a class="collapse-item" href="modals.html">Modals</a>
-            <a class="collapse-item" href="popovers.html">Popovers</a>
-            <a class="collapse-item" href="progress-bar.html">Progress Bars</a>
-          </div>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseForm" aria-expanded="true"
-          aria-controls="collapseForm">
-          <i class="fab fa-fw fa-wpforms"></i>
-          <span>Forms</span>
-        </a>
-        <div id="collapseForm" class="collapse" aria-labelledby="headingForm" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Forms</h6>
-            <a class="collapse-item" href="form_basics.html">Form Basics</a>
-            <a class="collapse-item" href="form_advanceds.html">Form Advanceds</a>
-          </div>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTable" aria-expanded="true"
-          aria-controls="collapseTable">
-          <i class="fas fa-fw fa-table"></i>
-          <span>Tables</span>
-        </a>
-        <div id="collapseTable" class="collapse" aria-labelledby="headingTable" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Tables</h6>
-            <a class="collapse-item" href="simple-tables.html">Simple Tables</a>
-            <a class="collapse-item" href="datatables.html">DataTables</a>
-          </div>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="ui-colors.html">
-          <i class="fas fa-fw fa-palette"></i>
-          <span>UI Colors</span>
-        </a>
-      </li>
-      <hr class="sidebar-divider">
-      <div class="sidebar-heading">
-        Examples
-      </div>
-      <li class="nav-item active">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePage" aria-expanded="true"
-          aria-controls="collapsePage">
-          <i class="fas fa-fw fa-columns"></i>
-          <span>Pages</span>
-        </a>
-        <div id="collapsePage" class="collapse show" aria-labelledby="headingPage" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Example Pages</h6>
-            <a class="collapse-item" href="login.html">Login</a>
-            <a class="collapse-item" href="register.html">Register</a>
-            <a class="collapse-item" href="404.html">404 Page</a>
-            <a class="collapse-item active" href="blank.html">Blank Page</a>
-          </div>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-          <i class="fas fa-fw fa-chart-area"></i>
-          <span>Charts</span>
-        </a>
-      </li>
-      <hr class="sidebar-divider">
-      <div class="version" id="version-ruangadmin"></div>
-    </ul>
-    <!-- Sidebar -->
+<!-- Sidebar -->
+<ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <div class="sidebar-brand-icon">
+      <img src="RuangAdmin/img/logo/logo2.png">
+    </div>
+    <div class="sidebar-brand-text mx-3">STARS</div>
+  </a>
+
+  <hr class="sidebar-divider my-0">
+
+  <!-- Dashboard -->
+  <li class="nav-item">
+    <a class="nav-link" href="dashboard.html">
+      <i class="fas fa-fw fa-tachometer-alt"></i>
+      <span>Dashboard</span></a>
+  </li>
+
+  <hr class="sidebar-divider">
+
+  <!-- Manajemen Pengguna -->
+  <div class="sidebar-heading">Manajemen Pengguna</div>
+
+  <li class="nav-item">
+    <a class="nav-link" href="kelola-mahasiswa.html">
+      <i class="fas fa-fw fa-user-graduate"></i>
+      <span>Kelola Mahasiswa</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="kelola-dosen.html">
+      <i class="fas fa-fw fa-chalkboard-teacher"></i>
+      <span>Kelola Dosen</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="kelola-admin.html">
+      <i class="fas fa-fw fa-user-cog"></i>
+      <span>Kelola Admin</span>
+    </a>
+  </li>
+
+  <hr class="sidebar-divider">
+
+  <!-- Manajemen Prestasi -->
+  <div class="sidebar-heading">Manajemen Prestasi</div>
+
+  <li class="nav-item">
+    <a class="nav-link" href="verifikasi-prestasi.html">
+      <i class="fas fa-fw fa-check-circle"></i>
+      <span>Verifikasi Prestasi</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="laporan-analisis.html">
+      <i class="fas fa-fw fa-chart-line"></i>
+      <span>Laporan & Analisis</span>
+    </a>
+  </li>
+
+  <hr class="sidebar-divider">
+
+  <!-- Manajemen Lomba -->
+  <div class="sidebar-heading">Manajemen Lomba</div>
+
+  <li class="nav-item">
+    <a class="nav-link" href="verifikasi-lomba.html">
+      <i class="fas fa-fw fa-clipboard-check"></i>
+      <span>Verifikasi Lomba</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="kelola-lomba.html">
+      <i class="fas fa-fw fa-trophy"></i>
+      <span>Kelola Lomba</span>
+    </a>
+  </li>
+
+  <hr class="sidebar-divider">
+
+  <!-- Manajemen Akademik -->
+  <div class="sidebar-heading">Manajemen Akademik</div>
+
+  <li class="nav-item">
+    <a class="nav-link" href="periode-semester.html">
+      <i class="fas fa-fw fa-calendar-alt"></i>
+      <span>Periode Semester</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="program-studi.html">
+      <i class="fas fa-fw fa-university"></i>
+      <span>Program Studi</span>
+    </a>
+  </li>
+
+  <hr class="sidebar-divider">
+
+  <div class="version" id="version-ruangadmin"></div>
+</ul>
+<!-- End of Sidebar -->
